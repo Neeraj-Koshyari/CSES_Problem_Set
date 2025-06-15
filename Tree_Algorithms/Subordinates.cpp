539b04eef73f2fb932aa51172e1e8a48caf0208b
Neeraj-Koshyari <140335273+Neeraj-Koshyari@users.noreply.github.com>
2025-06-15 08:36:21 +0530

    Update Subordinates.cpp

--- Tree_Algorithms/Subordinates.cpp
+++ Tree_Algorithms/Subordinates.cpp
@@ -1,3 +1,5 @@
+// c++
+
 #include<bits/stdc++.h>
 #define ll long long
 using namespace std;
@@ -33,4 +35,56 @@ int main(){
 	cout<<endl;
  
     return 0;
+}
+
+
+// java
+public class Main{
+    public static void main(String[] args) throws IOException {
+        FastReader sc = new FastReader();
+        FastWriter so = new FastWriter();
+
+
+        int n = sc.nextInt();
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        int count[] = new int[n];
+
+        for(int i=0; i<n; i++)  adj.add(new ArrayList<>());
+
+        for(int i=1; i<n; i++){
+            int x = sc.nextInt() - 1;
+            adj.get(x).add(i);
+        }
+
+        Deque<Integer> dq = new ArrayDeque<>();
+        Deque<Boolean> ready = new ArrayDeque<>();
+
+        dq.addFirst(0);
+        ready.addFirst(false);
+
+        while(!dq.isEmpty()){
+            int u = dq.removeFirst();
+            boolean check = ready.removeFirst();
+
+            if(!check){
+                dq.addFirst(u);
+                ready.addFirst(true);
+
+                for(int x : adj.get(u)){
+                    dq.addFirst(x);
+                    ready.addFirst(false);
+                }
+            }
+            else{
+                for(int x : adj.get(u)){
+                    count[u] += count[x]+1;
+                }
+            }
+        }
+        
+
+        for(int x : count){
+            so.print(x + " ");
+        }
+    }
 }
